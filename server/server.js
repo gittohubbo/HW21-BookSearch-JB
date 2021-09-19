4851afed4a20ee2d7b5fd4ba1ab83abb925b5184
Adam Jones <jbensus@gmail.com>
2021-09-18 21:12:48 -0500

    Changed port settings on server file

--- server/server.js
+++ server/server.js
@@ -36,6 +36,7 @@ app.use(routes);
 
 db.once('open', () => {
   app.listen(PORT, () => {
-    console.log(`🌍 Now listening on localhost:${PORT}`));
+    console.log(`🌍 Now listening on localhost:${PORT}!`);
     console.log(`Use GraphQL at http://localhost:${PORT}${server.graphqlPath}`);
+});
 });
